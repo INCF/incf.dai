7b80905f74a66cef7331dcdb30e1d7c6f065ced0
ritz <ritz@0d740851-f696-4369-8839-c524ab81679a>
2010-06-24 14:38:14 +0000

    Some hard-coded base URLs for the time being
    git-svn-id: http://svn.incf.org/svn/incfdai@9 0d740851-f696-4369-8839-c524ab81679a

---
+++ trunk/incf/dai/config.py
@@ -0,0 +1,7 @@
+# Hardcoded meta data as long as we don't get it from the central hub
+
+HUBS = {'aba'  :'http://whs0.pdc.kth.se:8080/incf-services/service/ABAServiceController',
+        'emage':'http://whs0.pdc.kth.se:8080/incf-services/service/EmageServiceController',
+        'ucsd' :'http://whs0.pdc.kth.se:8080/incf-services/service/UCSDServiceController',
+        'whs'  :'http://whs0.pdc.kth.se:8080/incf-services/service/WHSServiceController',
+        }
