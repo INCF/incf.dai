804f41c3cbac288ef9961aa0a632fe59a0dad7fe
ritz <ritz@0d740851-f696-4369-8839-c524ab81679a>
2010-09-02 08:58:48 +0000

    emage is emap ...
    git-svn-id: http://svn.incf.org/svn/incfdai@26 0d740851-f696-4369-8839-c524ab81679a

--- trunk/incf/dai/config.py
+++ trunk/incf/dai/config.py
@@ -1,7 +1,7 @@
 # Hardcoded meta data as long as we don't get it from the central hub
 
 HUBS = {'aba'  :'http://incf-dev.crbs.ucsd.edu:8080/atlas-aba?service=WPS',
-        'emage':'http://incf-dev.crbs.ucsd.edu:8080/atlas-emage?service=WPS',
+        'emap' :'http://incf-dev.crbs.ucsd.edu:8080/atlas-emap?service=WPS',
         'ucsd' :'http://incf-dev.crbs.ucsd.edu:8080/atlas-ucsd?service=WPS',
         'whs'  :'http://incf-dev.crbs.ucsd.edu:8080/atlas-whs?service=WPS',
         }
