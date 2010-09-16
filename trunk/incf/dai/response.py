163ae15d3461ee41d2a0a844b90822fce5320f79
raphael <raphael@0d740851-f696-4369-8839-c524ab81679a>
2010-09-16 09:29:24 +0000

    Enhancing the response object with a 'keys' method
    git-svn-id: http://svn.incf.org/svn/incfdai@40 0d740851-f696-4369-8839-c524ab81679a

--- trunk/incf/dai/response.py
+++ trunk/incf/dai/response.py
@@ -1,19 +1,26 @@
+"""Custom response object including parsed data fro xml responses"""
+
 import xml.dom.minidom
 
 from incf.dai.utils import xml2obj
 
 class Response(object):
-    """Top-level response object"""
+    """Custom response object including the parsed response in self.data"""
 
     def __init__(self, response):
         self.headers = response[0]
         self.content = response[1]
-        self._dict = xml2obj(response[1])
+        self.data = xml2obj(response[1])
 
     def __getitem__(self, key):
-        return self._dict[key]
+        return self.data[key]
     
     def __str__(self):
         doc = xml.dom.minidom.parseString(self.content)
         return doc.toprettyxml()
 
+    def keys(self):
+        """keys of the parsed data - if available"""
+        if self.data is None:
+            return []
+        return self.data.keys()
