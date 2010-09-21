5a6c20cd6e81e15a1539f0980f66287de8d7718b
raphael <raphael@0d740851-f696-4369-8839-c524ab81679a>
2010-09-21 07:40:40 +0000

    Move loger definition to config file
    git-svn-id: http://svn.incf.org/svn/incfdai@65 0d740851-f696-4369-8839-c524ab81679a

--- trunk/incf/dai/config.py
+++ trunk/incf/dai/config.py
@@ -5,3 +5,26 @@ HUBS = {'aba'  :'http://incf-dev.crbs.ucsd.edu:8080/atlas-aba?service=WPS',
         'ucsd' :'http://incf-dev.crbs.ucsd.edu:8080/atlas-ucsd?service=WPS',
         'whs'  :'http://incf-dev.crbs.ucsd.edu:8080/atlas-whs?service=WPS',
         }
+
+
+# set up logging to file - straight from the Python docs
+
+import logging
+
+LOG_FILENAME = "./incf.dai.log"
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-8s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M',
+                    filename=LOG_FILENAME,
+                    filemode='a')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-8s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+LOGGER = logging.getLogger('incf.dai')
--- trunk/incf/dai/hub.py
+++ trunk/incf/dai/hub.py
@@ -2,28 +2,9 @@
 
 import urllib
 import httplib2
-import logging
 
+from incf.dai.config import LOGGER
 from incf.dai.response import Response
-
-# set up logging to file - straight from the Python docs
-LOG_FILENAME = "./incf.dai.log"
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-8s %(levelname)-8s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M',
-                    filename=LOG_FILENAME,
-                    filemode='a')
-# define a Handler which writes INFO messages or higher to the sys.stderr
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-# set a format which is simpler for console use
-formatter = logging.Formatter('%(name)-8s: %(levelname)-8s %(message)s')
-# tell the handler to use this format
-console.setFormatter(formatter)
-# add the handler to the root logger
-logging.getLogger('').addHandler(console)
-
-LOGGER = logging.getLogger('incf.dai')
 
 
 class HubProxy(object):
