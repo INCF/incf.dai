fe7ee85b61b527e37781332a3a195b66180abf50
raphael <raphael@0d740851-f696-4369-8839-c524ab81679a>
2010-09-24 13:22:26 +0000

    Adjust version, license and keywords in setup.py
    git-svn-id: http://svn.incf.org/svn/incfdai@96 0d740851-f696-4369-8839-c524ab81679a

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4'
+version = '0.6'
 
 setup(name='incf.dai',
       version=version,
@@ -11,12 +11,13 @@ setup(name='incf.dai',
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
+      keywords='WPS INCF "Digital Atlasing"',
       author='Raphael Ritz',
       author_email='raphael.ritz@incf.org',
       url='http://svn.incf.org/svn/incfdai/',
-      license='BSD',
+      license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['incf'],
       include_package_data=True,
